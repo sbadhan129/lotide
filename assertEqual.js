4e72544058c344e4d12af01f12ca7bbd892fcd4f
Sourav <souravbadhan909@gmail.com>
2023-04-22 21:31:11 -0400

    Assertion program trial

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+var assertEqual = function(actual, expected) {
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+console.assert ( "Lighthouse Labs"==="Lighthouse Labs"); 
