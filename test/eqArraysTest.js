fa77f73a10d136b5878144af4904204ed3510a00
Sourav <souravbadhan909@gmail.com>
2023-05-13 00:06:12 -0400

    REFACTOR TEST CODES of eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+const tail = require('../tail');
+const eqArrays = require('../tail');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+console.assert ( "Lighthouse Labs"==="Lighthouse Labs"); 
