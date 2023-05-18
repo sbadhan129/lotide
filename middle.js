b10899d4b4b06b917ceb5b88e8d07b22c0b0f970
Sourav <souravbadhan909@gmail.com>
2023-05-18 07:03:42 -0400

    Refactor middle.js

--- middle.js
+++ middle.js
@@ -1,22 +1,4 @@
-var assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-var eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertEqual = require('./assertEqual');
 
 const middle = function(array) {
   let middleElements = [];
@@ -31,11 +13,4 @@ const middle = function(array) {
   return middleElements;
 };
 
-// TEST CODE
-assertArraysEqual("Lighthouse Labs", "Bootcamp");
-assertArraysEqual(1, 1);
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-console.assert("Lighthouse Labs" === "Lighthouse Labs");
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
